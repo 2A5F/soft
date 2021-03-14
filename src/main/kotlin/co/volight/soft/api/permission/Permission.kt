6f62ba595bfb15c2bc2cd4665c0b3a65c9b4817f
2A5F <U2A5F@outlook.com>
2021-03-14 21:52:08 +0800

    Permission

--- src/main/kotlin/co/volight/soft/api/permission/Permission.kt
+++ src/main/kotlin/co/volight/soft/api/permission/Permission.kt
@@ -5,13 +5,14 @@ import net.minecraft.entity.player.PlayerEntity
 import java.util.*
 
 interface Permission {
-    fun has(player: UUID, permission: String): Boolean
+    fun has(player: PlayerEntity, permission: String): Boolean
     fun has(subject: Subject, permission: String) = subject.hasPermission(permission)
 
     companion object: Permission {
         private val impl = Service.service<Permission>()
 
-        override fun has(player: UUID, permission: String) = impl.get().has(player, permission)
+        override fun has(player: PlayerEntity, permission: String) = impl.get().has(player, permission)
+        override fun has(subject: Subject, permission: String) = impl.get().has(subject, permission)
     }
 }
 
@@ -19,4 +20,4 @@ interface Subject {
     fun hasPermission(permission: String): Boolean
 }
 
-fun PlayerEntity.hasPermission(permission: String) = Permission.has(this.uuid, permission)
+fun PlayerEntity.hasPermission(permission: String) = Permission.has(this, permission)
